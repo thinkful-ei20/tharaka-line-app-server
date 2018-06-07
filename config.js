442be1ab2120a80842976c93973f45559cabfb2d
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-06-07 08:43:58 -0700

    config file fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/line-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/line-app';//put DATABASE_URL in heroku
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/line-app';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
